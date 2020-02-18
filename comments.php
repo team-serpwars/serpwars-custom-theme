91fa731dfeb91ee171d455cc1a53e75756a0bcf8
noobertx <noobertx@gmail.com>
2020-02-18 11:38:27 +0800

    Updated Base Theme

--- comments.php
+++ comments.php
@@ -4,13 +4,15 @@
  *
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
+ *
+ * @package HelloElementor
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
-// Comment Reply Script
+// Comment Reply Script.
 if ( comments_open() && get_option( 'thread_comments' ) ) {
 	wp_enqueue_script( 'comment-reply' );
 }
@@ -19,24 +21,42 @@ if ( comments_open() && get_option( 'thread_comments' ) ) {
 
 	<?php if ( have_comments() ) : ?>
 		<h3 class="title-comments">
-			<span>
-				<?php printf( _n( 'One Response', '%1$s Responses', get_comments_number(), 'elementor-hello-theme' ), number_format_i18n( get_comments_number() ), get_the_title() ); ?>
-			</span>
+			<?php
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				printf( esc_html_x( 'One Response', 'comments title', 'hello-elementor' ) );
+			} else {
+				printf(
+					esc_html( /* translators: 1: number of comments */
+						_nx(
+							'%1$s Response',
+							'%1$s Responses',
+							$comments_number,
+							'comments title',
+							'hello-elementor'
+						)
+					),
+					esc_html( number_format_i18n( $comments_number ) )
+				);
+			}
+			?>
 		</h3>
 
-	<?php the_comments_navigation(); ?>
+		<?php the_comments_navigation(); ?>
 
 	<ol class="comment-list">
 		<?php
-		wp_list_comments( array(
-			'style'       => 'ol',
-			'short_ping'  => true,
-			'avatar_size' => 42,
-		) );
+		wp_list_comments(
+			array(
+				'style'       => 'ol',
+				'short_ping'  => true,
+				'avatar_size' => 42,
+			)
+		);
 		?>
 	</ol><!-- .comment-list -->
 
-	<?php the_comments_navigation(); ?>
+		<?php the_comments_navigation(); ?>
 
 <?php endif; // Check for have_comments(). ?>
 
@@ -44,14 +64,16 @@ if ( comments_open() && get_option( 'thread_comments' ) ) {
 // If comments are closed and there are comments, let's leave a little note, shall we?
 if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-	<p class="no-comments"><?php _e( 'Comments are closed.', 'elementor-hello-theme' ); ?></p>
+	<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'hello-elementor' ); ?></p>
 <?php endif; ?>
 
 <?php
-comment_form( array(
-	'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-	'title_reply_after'  => '</h2>',
-) );
+comment_form(
+	array(
+		'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+		'title_reply_after'  => '</h2>',
+	)
+);
 ?>
 
 </section><!-- .comments-area -->
