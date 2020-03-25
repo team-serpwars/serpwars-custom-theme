70c9dff29a0b1190a5036d24e99c2f7a5209574d
noobertx <noobertx@gmail.com>
2020-03-25 11:19:14 +0800

    Install ACF Revision for PRO

--- inc/admin/modules/serpwars-theme.demo-importer.php
+++ inc/admin/modules/serpwars-theme.demo-importer.php
@@ -103,11 +103,11 @@
             $data    = $this->get_demo_data();
             $theme_options = get_option("serpwars_theme_options");
 
-
             if(isset($data['acf'])){
                 $ids = array();
                 foreach($data['acf'] as $index => $acf){
                     if($theme_options["acf"][$index]->id == 0 || !get_post($theme_options["acf"][$index]->id)){ //Item has id of 0 or if it does not really exist at all
+                       if(function_exists ('acf_get_field_group_post')){                
                         $post = acf_get_field_group_post( $acf['key'] );
                         if( $post ) {
                             $acf['ID'] = $post->ID;
@@ -121,10 +121,41 @@
                         $acf = acf_import_field_group( $acf );
                         $ids[] = $acf['ID'];
                         $total = count($ids);
-                        $text = sprintf( _n( 'Imported 1 field group', 'Imported %s field groups', $total, 'acf' ), $total );                       
-                       
+                        $text = sprintf( _n( 'Imported 1 field group', 'Imported %s field groups', $total, 'acf' ), $total );   
 
-                    }              
+                       }else{
+
+                        $field_group = acf_get_local_field_group( $acf  );
+
+                        // attempt get id
+            $id = acf_maybe_get( $ids, $key );
+            
+            if( $id ) {
+                
+                $field_group['ID'] = $id;
+                
+            }
+            
+            
+            // append fields
+            if( acf_have_local_fields($key) ) {
+                
+                $field_group['fields'] = acf_get_local_fields( $key );
+                
+            }
+            
+            
+            // import
+            $field_group = acf_import_field_group( $field_group );
+           
+
+
+                       }                                                                
+                    }
+
+            $theme_options["acf"][$index]->ID = $field_group["ID"];           
+            $theme_options["acf"][$index]->title = $acf["title"];           
+            $theme_options["acf"][$index]->found = true;           
                 
                 }
 
@@ -133,7 +164,7 @@
             update_option("serpwars_theme_options",$theme_options );
 
             }
-
+            $this->import_options();
             wp_send_json_success($theme_options);
         }
         public function import_options() {
